5109bd8de555b5d2179a00a97da596dd8fc9a476
Jadson Anderson <jadson.anderson.alves.ferreira@gmail.com>
2026-07-08 17:22:01 -0300

    Pequena atualização no Retrofit

--- android_app/app/src/main/java/com/example/bolaocopaapp/data/network/RetrofitClient.kt
+++ android_app/app/src/main/java/com/example/bolaocopaapp/data/network/RetrofitClient.kt
@@ -5,7 +5,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     // 10.0.2.2 é o endereço que o emulador do Android usa para acessar o localhost do PC.
-    // Se vocês forem testar em um celular físico via USB mais tarde, precisarão colocar o IP local da sua máquina aqui (ex: 192.168.x.x).
     private const val BASE_URL = "http://10.0.2.2:8000/"
 
     val apiService: BolaoAPIService by lazy {
